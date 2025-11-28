04c41f9fe0311f2483e228fb3fa4aec2416411a3
NamaAnda <julianarwansahh@gmail.com>
2025-11-28 21:22:30 +0700

    menambahkan code di dalam note_model.dart

---
+++ lib/models/note_model.dart
@@ -0,0 +1,21 @@
+class Note {
+  final int? id;
+  final String title;
+  final String content;
+
+  Note({this.id, required this.title, required this.content});
+
+  factory Note.fromMap(Map<String, dynamic> json) => Note(
+        id: json['id'],
+        title: json['title'] ?? '',
+        content: json['content'] ?? '',
+      );
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'title': title,
+      'content': content,
+    };
+  }
+}
